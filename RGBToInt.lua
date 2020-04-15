c8bae5cb6b525e1649d2501e60e779e047f4d015
rossgrams <ross.grams@gmail.com>
2020-04-15 09:32:20 -0400

    RGBToInt: Just return function rather than table with __call.

--- RGBToInt.lua
+++ RGBToInt.lua
@@ -10,13 +10,11 @@ end
 -- 	Inputs are clamped to 0-255.
 -- 	Can use a single input to get a grey color of that value.
 -- 	Will return black if called with no inputs.
-local function RGBToInt(_, r, g, b)
+local function RGBToInt(r, g, b)
 	r = r and clamp(r, 0, 255) or 0
 	g = g and clamp(g, 0, 255) or r
 	b = b and clamp(b, 0, 255) or r
 	return r + g * 256 + b * 65536
 end
 
-local mt = { __call = RGBToInt }
-
-return setmetatable({}, mt)
+return RGBToInt
