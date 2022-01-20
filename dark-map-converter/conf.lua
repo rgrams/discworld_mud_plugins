a962a08ee82308a3b874c3361f492946019621da
Ross Grams <ross.grams@gmail.com>
2022-01-20 18:32:58 -0500

    Map-Converter: Add makelove file and .gitignore

--- dark-map-converter/conf.lua
+++ dark-map-converter/conf.lua
@@ -11,7 +11,7 @@ function love.conf(t)
 	t.audio.mic = false                 -- Request and use microphone capabilities in Android (boolean)
 	t.audio.mixwithsystem = true        -- Keep background music playing when opening LOVE (boolean, iOS and Android only)
 
-	t.window.title = "Discworld MUD Map Converter"         -- The window title (string)
+	t.window.title = "Dark Map Converter 1.0"         -- The window title (string)
 	t.window.icon = nil                 -- Filepath to an image to use as the window's icon (string)
 	t.window.width = 800                -- The window width (number)
 	t.window.height = 600               -- The window height (number)
